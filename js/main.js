619e8a6d463678ed6a22b41a63e8d089b08294f7
Lidor Waldman <lidorvl@gmail.com>
2022-03-02 11:31:27 +0200

    added vue libraries changed main.js to work with libs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-"use strict";
+import mainHeader from "./cmps/main-header.cmp.js";
+import mainFooter from "./cmps/main-footer.cmp.js";
+
+const options = {
+    components: {
+        mainHeader,
+        mainFooter
+    },
+    template: `
+    <section class="app">
+        <main-header />
+        <!-- <router-view />  -->
+        <main-footer />
+    </section>
+    `,
+
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+const app = Vue.createApp(options);
+app.use(router);
+app.mount('#app');
