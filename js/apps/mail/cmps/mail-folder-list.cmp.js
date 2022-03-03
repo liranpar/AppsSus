ff8c2d41544a565c11d9e9b4d8b1b718268705c6
Liran Parti <liranpar@gmail.com>
2022-03-03 15:41:49 +0200

    text13

--- js/apps/mail/cmps/mail-folder-list.cmp.js
+++ js/apps/mail/cmps/mail-folder-list.cmp.js
@@ -1,27 +1,28 @@
 export default {
-    template: `
+  template: `
         <section class="mail-folder-list">
             <ul class="mail-folder-list-ul">
-                <li class="folder-list-inbox"><i class="fas fa-inbox">Inbox</i></li> <!--<i class="fas fa-inbox">Inbox</i> -->
-                <li class="folder-list-starred">Starred</li>
-                <li class="folder-list-sent-mail">Sent Mail</li>
-                <li class="folder-list-drafts">Drafts</li>
+                <li @click="setFilter('inbox')" class="folder-list-inbox"><i class="fas fa-inbox">Inbox</i></li> <!--<i class="fas fa-inbox">Inbox</i> -->
+                <li @click="setFilter('starred')"  class="folder-list-starred">Starred</li>
+                <li @click="setFilter('sent')"  class="folder-list-sent-mail">Sent Mail</li>
+                <li @click="setFilter('drafts')" class="folder-list-drafts">Drafts</li>
             </ul>
         </section>
 `,
-    data() {
-        return {
-
-        }
+  data() {
+    return {
+      filterBy: {
+        folder: "inbox",
+        text: "",
+      },
+    };
+  },
+  created() {},
+  methods: {
+    setFilter(val) {
+      this.filterBy.folder = val;
+      this.$emit("setFilter", { ...this.filterBy });
     },
-    created() {
-
-    },
-    methods: {
-
-    },
-    computed: {
-
-    }
-
-}
+  },
+  computed: {},
+};
--- js/apps/mail/pages/mail-index.cmp.js
+++ js/apps/mail/pages/mail-index.cmp.js
@@ -4,36 +4,42 @@ import mailFolderList from "../cmps/mail-folder-list.cmp.js";
 import mailCompose from "../cmps/mail-compose.cmp.js";
 
 export default {
-    template: `
+  template: `
         <section class="main-mail-page">
             <section class="compose-and-folder">
                 <mail-compose />
-                <mail-folder-list />
+                <mail-folder-list @setFilter="setFilter" />
             </section>
-            <mail-list v-if="mails" :mails="mails" />
+            <mail-list v-if="mails" :mails="mailsToDisplay" />
         </section>
 `,
-components: {
+  components: {
     mailService,
     mailList,
     mailFolderList,
     mailCompose,
-
-},
-    data() {
-        return {
-            mails: [],
-        }
+  },
+  data() {
+    return {
+      mails: [],
+      filterBy: {
+        folder: "inbox",
+        text: "",
+      },
+    };
+  },
+  created() {
+    mailService.query().then((mails) => (this.mails = mails));
+  },
+  methods: {
+    setFilter(filterByFromFolders) {
+      this.filterBy = filterByFromFolders;
     },
-    created() {
-        mailService.query()
-            .then(mails => this.mails = mails);
+  },
+  computed: {
+    mailsToDisplay() {
+      return this.mails;
+      //   return this.mails.filter((mail) => mail.status === this.filterBy.folder);
     },
-    methods: {
-
-    },
-    computed: {
-
-    }
-
-}
+  },
+};
