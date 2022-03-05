4563e6e80a7102aa147ee635454015793f0a892d
Lidor Waldman <lidorvl@gmail.com>
2022-03-05 15:29:50 +0200

    added homepage

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -1,6 +1,14 @@
 export default {
 template: `
+    <section class="home-page">
+        <section class="about">
+            <section class="about-images">
+                <img src="../assets/images/lidor.jpg">
+                <img src="../assets/images/liran.jpg">
+            </section>
+        </section>
 
+    </section>
 `,
 data() {
 return {
