4051d79599d1c489a59dd11b9183cfe78c48181d
Lidor Waldman <95521150+LidorWa@users.noreply.github.com>
2022-03-05 19:56:41 +0200

    changed images path to work in github

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -6,9 +6,9 @@ template: `
         </section>
         <h1 class="pick-app-title">Pick your favorite App:</h1>
             <ul class="apps-images-ul">
-                <li><router-link to="/keep"><img src="../assets/images/keep.jpeg" title="ApsusKeep"></router-link></li>
-                <li><router-link to="/mail"><img src="../assets/images/email.png" title="ApsusMail"></router-link></li>
-                <li><router-link to="/book"><img src="../assets/images/books.jpg" title="ApsusBooks"></router-link></li>
+                <li><router-link to="/keep"><img src="assets/images/keep.jpeg" title="ApsusKeep"></router-link></li>
+                <li><router-link to="/mail"><img src="assets/images/email.png" title="ApsusMail"></router-link></li>
+                <li><router-link to="/book"><img src="assets/images/books.jpg" title="ApsusBooks"></router-link></li>
             </ul>
 
 
