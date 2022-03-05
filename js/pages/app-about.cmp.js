bee7b6814ff4b97ebe134949607748333b298fb9
Liran Parti <liranpar@gmail.com>
2022-03-05 17:56:07 +0200

    a

--- js/pages/app-about.cmp.js
+++ js/pages/app-about.cmp.js
@@ -1,20 +1,15 @@
 export default {
-template: `
+  template: `
+<section class="about-cont" >
+    <div class="about-div">aaaaa</div>
+    <div class="about-div">bbbbb</div>
+</section>
 
 `,
-data() {
-return {
-
-}
-},
-created() {
-
-},
-methods: {
-
-},
-computed: {
-
-}
-
-}
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+  computed: {},
+};
