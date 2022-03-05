305e1eb62104cf51b3d25f0cb0f04a0b86dbdc40
Liran Parti <liranpar@gmail.com>
2022-03-05 19:43:30 +0200

    final version

--- js/pages/app-about.cmp.js
+++ js/pages/app-about.cmp.js
@@ -1,8 +1,32 @@
 export default {
   template: `
-<section class="about-cont" >
-    <div class="about-div">aaaaa</div>
-    <div class="about-div">bbbbb</div>
+<section class="aboute-page"  >
+    <h1 >About Appsus</h1>
+    <p class="aboute-page-p">
+    Our mission is to organize the world’s information and make it universally accessible and useful. There's this notion that to grow a business, you have to be ruthless. But we know there's a better way to grow. One where what's good for the bottom line is also good for customers. We believe businesses can grow with a conscience, and succeed with a soul — and that they can do it with inbound. That's why we've created an ecosystem uniting aplication.
+    </p>
+    <h2>Meet the founders</h2>
+    <div class="about-cont" >
+
+
+        <div class="about-div">
+            <p class="about-div-p">Liran Parti</p>
+            <p class="about-div-p role" >CTO</p>
+            <div class="image-div" >
+                <img src="../assets/images/liran.jpg" alt="">
+            </div>
+            <p class="description" >Liran is an outstanding student at the leading college Codding-Accademy, and a future promising Full Stack Developer.</p>
+        </div>
+        <div class="about-div">
+            <p class="about-div-p">Lidor Waldman</p>
+            <p class="about-div-p role">CEO</p>
+            <div class="image-div" >
+                <img src="../assets/images/lidor.jpg" alt="">
+            </div>
+            <p class="description" >Lidor is a leading troll. He likes beer, hamburger, router links and flex directions. Master at event bus features.</p>
+        </div>
+    </div>
+    
 </section>
 
 `,
