f28b9e7704a12546e54ffc1d291a56c8342da222
Liran Parti <liranpar@gmail.com>
2022-03-05 15:36:25 +0200

    a

--- js/cmps/hamburger.cmp.js
+++ js/cmps/hamburger.cmp.js
@@ -5,14 +5,14 @@ export default {
           <i class="fa fa-th" aria-hidden="true" @click="isClicked = true" ></i>
                <div class="hamburger-menu" :class="{ showMenu: isClicked }" @mouseleave="isClicked = false" >
                    <div class="apps-links-cont">
-                       <span><router-link @click="isClicked = false" class="mail-link" to="/mail">✉</router-link></span>
-                       <span><router-link @click="isClicked = false" class="keep-link" to="/keep">📌</router-link></span>
-                       <span><router-link @click="isClicked = false" class="keep-link" to="/book">📚</router-link></span>
+                       <span><router-link @click="isClicked = false" class="link mail-link" to="/mail">✉</router-link></span>
+                       <span><router-link @click="isClicked = false" class="link keep-link" to="/keep">📌</router-link></span>
+                       <span><router-link @click="isClicked = false" class="link keep-link" to="/book">📚</router-link></span>
                    </div>
                    
-                   <span><router-link @click="isClicked = false" to="/">Home</router-link></span>
+                   <span><router-link class="link" @click="isClicked = false" to="/">Home</router-link></span>
                    <span> | </span>
-                   <span><router-link @click="isClicked = false" to="/about">About</router-link></span>
+                   <span><router-link class="link" @click="isClicked = false" to="/about">About</router-link></span>
                </div>
           </section>
     
