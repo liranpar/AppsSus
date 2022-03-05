caa8b640a839608d766e7252bdd28774485bcb46
Liran Parti <liranpar@gmail.com>
2022-03-05 11:32:50 +0200

    a

--- js/apps/mail/cmps/mail-folder-list.cmp.js
+++ js/apps/mail/cmps/mail-folder-list.cmp.js
@@ -2,11 +2,11 @@ export default {
   template: `
         <section class="mail-folder-list">
             <ul class="mail-folder-list-ul">
-                <li @click="setFilter('inbox')" class="folder-list-inbox">Inbox</li> <!--<i class="fas fa-inbox">Inbox</i> -->
-                <li @click="setFilter('starred')"  class="folder-list-starred">Starred</li>
-                <li @click="setFilter('sent')"  class="folder-list-sent-mail">Sent Mail</li>
-                <li @click="setFilter('drafts')" class="folder-list-drafts">Drafts</li>
-                <li @click="setFilter('removed')" class="folder-list-drafts">Deleted</li>
+                <li @click="setFilter('inbox')" class="folder-list-inbox" :class="{ folderClicked: isSelected('inbox')}" >Inbox</li> <!--<i class="fas fa-inbox">Inbox</i> -->
+                <li @click="setFilter('starred')"  class="folder-list-starred" :class="{ folderClicked: isSelected('inbox')}>Starred</li>
+                <li @click="setFilter('sent')"  class="folder-list-sent-mail" :class="{ folderClicked: isSelected('sent')}>Sent Mail</li>
+                <li @click="setFilter('drafts')" class="folder-list-drafts" :class="{ folderClicked: isSelected('drafts')}>Drafts</li>
+                <li @click="setFilter('removed')" class="folder-list-drafts" :class="{ folderClicked: isSelected('removed')}>Deleted</li>
             </ul>
         </section>
 `,
@@ -15,12 +15,20 @@ export default {
       filterBy: {
         folder: "inbox",
         text: "",
-        folderClicked: 'folder-clicked'
+        folderClicked: "folder-clicked",
       },
+      // isSelecedFolder: {
+      //   text: true,
+      //   image: false,
+      //   list: false,
+      // },
     };
   },
   created() {},
   methods: {
+    isSeleced(val) {
+      return this.filterBy.folder === val;
+    },
     setFilter(val) {
       this.filterBy.folder = val;
       this.$emit("setFilter", { ...this.filterBy });
